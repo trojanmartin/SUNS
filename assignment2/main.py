8da4acb225fa4c6aaeb99c11da3e2d2d60e01856
trojanmartin <trojanmartin@users.noreply.github.com>
2020-10-24 21:00:27 +0200

    removed outliers from data

--- assignment2/main.py
+++ assignment2/main.py
@@ -6,41 +6,74 @@ import tensorflow.keras as kr
 import matplotlib.pyplot as plt
 import seaborn as sns
 import tensorflow as tf
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
 
 
 def clear_data(df):
     df = df.dropna(how="any", axis=0)
     df = df.drop(columns=['track.id','track.name','track.artist','track.popularity','track.album.id','track.album.name', 'track.album.release_date','playlist_name','playlist_id', 'playlist_subgenre'])
     df['playlist_genre'] = df['playlist_genre'].map({'edm': 0, 'latin': 1, 'pop': 2, 'r&b': 3, 'rap': 4, 'rock': 5})
+
+    df.drop(df[df.tempo > 220].index, inplace=True)
+    df.drop(df[df.tempo < 50].index, inplace=True)
+
+    df.drop(df[df.key < 0.3].index, inplace=True)
+
+    df.drop(df[df.loudness < -30].index, inplace=True)
+    df.drop(df[df.speechiness > 0.8].index, inplace=True)
+
+    df.drop(df[df.duration_ms > 1].index, inplace=True)
+
+
     df['key']  = df['key'] / 11
 
     df['duration_ms'] = (df['duration_ms'] - df['duration_ms'].min()) / (df['duration_ms'].max() - df['duration_ms'].min())
     df['loudness'] = (df['loudness'] - df['loudness'].min()) / (df['loudness'].max() - df['loudness'].min())
     df['tempo'] = (df['tempo'] - df['tempo'].min())/(df['tempo'].max() - df['tempo'].min())
 
+
+
     return df
 
 def createGraphs(df):
-   fig = px.scatter(df, x="height", y="cardio")
-    #fig.show()
-    #fig = px.scatter(df, x="weight", y="cardio")
-    #fig.show()
-
-
-
-if __name__ == '__main__':
-    test_df = pd.read_csv("data/test.csv")
-    train_df = pd.read_csv("data/train.csv")
-
-    train_df = clear_data(train_df)
-    test_df = clear_data(test_df)
-
-    correlation_matrix = train_df.corr()
-    plt.figure(figsize=(10, 10))
-    sns.heatmap(correlation_matrix, vmax=1, square=True, annot=True, cmap='cubehelix')
+    plt.plot(df['tempo'])
+    plt.title('tempo')
     plt.show()
-#   createGraphs(train_df)
+    plt.plot(df['danceability'])
+    plt.title('danceability')
+    plt.show()
+    plt.plot(df['energy'])
+    plt.title('energy')
+    plt.show()
+    plt.plot(df['key'])
+    plt.title('key')
+    plt.show()
+    plt.plot(df['loudness'])
+    plt.title('loudness')
+    plt.show()
+    plt.plot(df['speechiness'])
+    plt.title('speechiness')
+    plt.show()
+    plt.plot(df['acousticness'])
+    plt.title('acousticness')
+    plt.show()
+    plt.plot(df['instrumentalness'])
+    plt.title('instrumentalness')
+    plt.show()
+    plt.plot(df['liveness'])
+    plt.title('liveness')
+    plt.show()
+    plt.plot(df['valence'])
+    plt.title('valence')
+    plt.show()
+    plt.plot(df['duration_ms'])
+    plt.title('duration_ms')
+    plt.show()
 
+
+def amn(train_df, test_df):
     train, validate = np.split(train_df.sample(frac=1), [int(.8 * len(train_df))])
 
     train_y = pd.get_dummies(train['playlist_genre'])
@@ -53,8 +86,8 @@ if __name__ == '__main__':
     test_x = test_df.drop(columns='playlist_genre')
 
     model = kr.Sequential()
-    model.add(kr.layers.Dense(100,input_dim=12, activation="sigmoid"))
-    model.add(kr.layers.Dense(50, activation="sigmoid"))
+    model.add(kr.layers.Dense(100, input_dim=12, activation="sigmoid"))
+    model.add(kr.layers.Dense(50, kernel_regularizer=kr.regularizers.L2(0.01), activation="sigmoid"))
     model.add(kr.layers.Dense(6, activation="sigmoid"))
 
     model.summary()
@@ -66,7 +99,7 @@ if __name__ == '__main__':
 
     predicted = np.argmax(model.predict(test_x), axis=1)
 
-    cm =  tf.math.confusion_matrix(test_y,predicted)
+    cm = tf.math.confusion_matrix(test_y, predicted)
     plt.figure(figsize=(10, 8))
     sns.heatmap(cm,
                 annot=True, fmt='g')
@@ -91,5 +124,46 @@ if __name__ == '__main__':
     plt.xlabel('epoch')
     plt.legend(['train', 'test'], loc='upper left')
     plt.show()
+
+
+def svm(train_df, test_df):
+
+    train_y = train_df['playlist_genre']
+    train_x = train_df.drop(columns='playlist_genre')
+
+#    svclassifier = SVC(kernel='poly', degree=8,C=0.5, verbose=False)
+#    svclassifier.fit(train_x, train_y)
+    print("Done")
+
+    C_range = np.logspace(-2, 10, 5)
+    gamma_range = np.logspace(-9, 3, 5)
+    param_grid = dict(gamma=gamma_range,C=C_range)
+
+    cv = StratifiedShuffleSplit(n_splits=3,test_size=0.2,random_state=42)
+    grid = GridSearchCV(SVC(verbose=1), param_grid=param_grid,cv=cv,verbose=1)
+    grid.fit(train_x, train_y)
+
+    scores = grid.cv_results_['mean_test_score'].reshape(len(C_range), len(gamma_range))
+
+    print("The bes %s with score %0.2f" % (grid.best_params_, grid.best_score_))
+
+if __name__ == '__main__':
+    test_df = pd.read_csv("data/test.csv")
+    train_df = pd.read_csv("data/train.csv")
+
+    train_df = clear_data(train_df)
+
+#  createGraphs(train_df)
+
+#    svm(train_df,test_df)
+
+    amn(train_df,test_df)
+    correlation_matrix = train_df.corr()
+    plt.figure(figsize=(10, 10))
+    sns.heatmap(correlation_matrix, vmax=1, square=True, annot=True, cmap='cubehelix')
+    plt.show()
+#   createGraphs(train_df)
+
+
 
     print("sad")
